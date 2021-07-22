710e93b36f0590c7039b7affac28a98c830a4e05
Mandy Yuan <myuan2024@gmail.com>
2021-07-21 21:48:33 -0500

    new hyperlink code

--- COMM Mobile Dev Final Project/ScrollViewController.swift
+++ COMM Mobile Dev Final Project/ScrollViewController.swift
@@ -9,13 +9,28 @@ import UIKit
 
 class ScrollViewController: UIViewController {
 
+    
+    @IBOutlet weak var blogLink: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let attributedString = NSMutableAttributedString (string: "Want to learn iOS? You should visit the best source of free iOS tutorials!")
+        attributedString.addAttribute(.link, value: "https://www.opendemocracy.net/en/can-europe-make-it/why-mutual-aid-social-solidarity-not-charity/", range: NSRange(location: 19, length: 55))
+
+        blogLink.attributedText = attributedString
         // Do any additional setup after loading the view.
     }
     
-
+    func blogLink(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+        UIApplication.shared.open(URL)
+        return false
+    }
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
