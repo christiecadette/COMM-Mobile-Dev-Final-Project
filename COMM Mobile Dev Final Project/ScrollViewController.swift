917ec9ad0b7b8cd811cded84badc19b2b3ac6482
Lorena Sosa <lorenasosaa98@gmail.com>
2021-07-22 10:38:11 -0400

    commit 1

--- COMM Mobile Dev Final Project/ScrollViewController.swift
+++ COMM Mobile Dev Final Project/ScrollViewController.swift
@@ -10,26 +10,23 @@ import UIKit
 class ScrollViewController: UIViewController {
 
     
-//    @IBOutlet weak var blogLink: UILabel!
-//    
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//     
-//        let attributedString = NSMutableAttributedString (string: "Blog Post Title")
-//        attributedString.addAttribute(.link, value: "https://www.opendemocracy.net/en/can-europe-make-it/why-mutual-aid-social-solidarity-not-charity/", range: NSRange(location: 19, length: 55))
-//
-//        blogLink.attributedText = attributedString
+    @IBOutlet weak var blogLink: UITextView!
+   
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let attributedString = NSMutableAttributedString (string: "Blog Post Title")
+      attributedString.addAttribute(.link, value: "https://www.opendemocracy.net/en/can-europe-make-it/why-mutual-aid-social-solidarity-not-charity/", range: NSRange(location: 19, length: 55))
+
+        blogLink.attributedText = attributedString}
 //
 //        
 //        // Do any additional setup after loading the view.
-//    }
-//    
-//    func blogLink(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
-//        UIApplication.shared.open(URL)
-//        return false
-//    }
+ func blogLink(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+    UIApplication.shared.open(URL)
+       return false
+   }
     
     
     
